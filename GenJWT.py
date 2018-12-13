e79d612f2d9377a8416061b4c62151e976439d60
Daiki Kojima <daikikojima0531@icloud.com>
2018-12-13 13:37:02 +0900

    A: add JWT Manager Class

---
+++ GenJWT.py
@@ -0,0 +1,47 @@
+import os
+from os.path import join, dirname
+from datetime import datetime, timedelta
+import requests
+import json
+import jwt
+from dotenv import load_dotenv
+
+class JwtManager():
+    # Initialize env file
+    def __init__(self):
+        dotenv_path = join(dirname(__file__), '.env')
+        load_dotenv(dotenv_path)
+
+    # Get PRIVATE KEY Of GitHub
+    def get_private_pem(self):
+        key = os.environ.get("PRIVATE_KEY","")
+        return key
+
+    def get_app_id(self):
+        return os.environ.get("APP_ID", 0)
+
+    # Get token of GitHub
+    def getToken(self, installation_id):
+        utcnow = datetime.utcnow() + timedelta(seconds=-5)
+        duration = timedelta(seconds=30)
+        payload = {
+            "iat": utcnow,
+            "exp": utcnow + duration,
+            "iss": self.get_app_id()
+        }
+        pem = self.get_private_pem()
+        encoded = jwt.encode(payload, pem, "RS256")
+        headers = {
+            "Authorization": "Bearer " + encoded.decode("utf-8"),
+            "Accept": "application/vnd.github.machine-man-preview+json"
+        }
+        auth_url = "https://api.github.com/installations/{}/access_tokens".format(installation_id)
+        r = requests.post(auth_url, headers=headers)
+
+        if not r.ok:
+            print(r.json()["message"])
+            r.raise_for_status()
+        token = r.json()["token"]
+        return {
+            "Authorization": "token {}".format(token)
+        }
